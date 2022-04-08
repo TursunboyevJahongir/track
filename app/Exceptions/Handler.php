a9f0e6e210f5ed57a66a95cc4bbdb30689fa30a5
TursunboyevJahongir <jah6332@gmail.com>
2022-04-09 03:40:39 +0500

    bugfix exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -17,7 +17,6 @@ use Spatie\Permission\Exceptions\PermissionAlreadyExists;
 use Spatie\Permission\Exceptions\RoleAlreadyExists;
 use Spatie\Permission\Exceptions\UnauthorizedException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 use TypeError;
 
@@ -28,7 +27,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var string[]
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -37,7 +36,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var string[]
+     * @var array<int, string>
      */
     protected $dontFlash = [
         'current_password',
@@ -52,6 +51,9 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 
     public function render($request, Throwable $e)
@@ -62,57 +64,72 @@ class Handler extends ExceptionHandler
             if (str_contains($message, 'No query results for model')) {
                 $message = 'No records found, try another request';
             }
-
-            return response()->json(['code' => 404,
-                'message' => $message,
-                'data' => []],
-                404);
+            return $request->expectsJson()
+                ? response()->json(['code' => 404,
+                    'message' => $message,
+                    'data' => []],
+                    404)
+                : abort(404, $message);
         }
         if ($e instanceof PermissionAlreadyExists) {
-            return response()->json(['code' => 422,
-                'message' => 'Permission already exists for this guard',
-                'data' => []],
-                422);
+            return $request->expectsJson()
+                ? response()->json(['code' => 422,
+                    'message' => "Permission already exists for this guard",
+                    'data' => []],
+                    422)
+                : abort(422, "Permission already exists for this guard");
         }
         if ($e instanceof AuthorizationException) {
-            return response()->json(['code' => 403,
-                'message' => $e->getMessage(),
-                'data' => []],
-                403);
+            return $request->expectsJson()
+                ? response()->json(['code' => 403,
+                    'message' => $e->getMessage(),
+                    'data' => []],
+                    403)
+                : abort(403, $e->getMessage());
         }
 
         if ($e instanceof UnauthorizedException) {
-            return response()->json(['code' => 403,
-                'message' => 'You dont have permissions to do this action',
-                'data' => []],
-                403);
+            return $request->expectsJson()
+                ? response()->json(['code' => 422,
+                    'message' => 'You dont have permissions to do this action',
+                    'data' => []],
+                    422)
+                : abort(422, "You dont have permissions to do this action");
         }
         if ($e instanceof RoleAlreadyExists) {
-            return response()->json(['code' => 422,
-                'message' => 'This role already exists',
-                'data' => []],
-                422);
+            return $request->expectsJson()
+                ? response()->json(['code' => 422,
+                    'message' => 'This role already exists',
+                    'data' => []],
+                    422)
+                : abort(422, "This role already exists");
         }
 
         if ($e instanceof TypeError) {
-            return response()->json(['code' => 500,
-                'message' => $e->getMessage(),
-                'data' => []],
-                500);
+            return $request->expectsJson()
+                ? response()->json(['code' => 500,
+                    'message' => $e->getMessage(),
+                    'data' => []],
+                    500)
+                : abort(500, $e->getMessage());
         }
 
         if ($e instanceof MethodNotAllowedHttpException) {
-            return response()->json(['code' => 405,
-                'message' => $e->getMessage(),
-                'data' => []],
-                405);
+            return $request->expectsJson()
+                ? response()->json(['code' => 405,
+                    'message' => $e->getMessage(),
+                    'data' => []],
+                    405)
+                : abort(405, $e->getMessage());
         }
 
         if ($e instanceof AuthenticationException) {
-            return response()->json(['code' => 401,
-                'message' => $e->getMessage(),
-                'data' => []],
-                401);
+            return $request->expectsJson()
+                ? response()->json(['code' => 401,
+                    'message' => $e->getMessage(),
+                    'data' => []],
+                    401)
+                : abort(401, $e->getMessage());
         }
         // End
 
@@ -143,12 +160,12 @@ class Handler extends ExceptionHandler
             return $this->convertValidationExceptionToResponse($e, $request);
         }
 
-        if ($e instanceof \Exception) {
-            return response()->json(['code' => 500,
-                'message' => $e->getMessage(),
-                'data' => $e->getTrace()],
-                500);
-        }
+//        if ($e instanceof \Exception) {
+//            return response()->json(['code' => 500,
+//                'message' => $e->getMessage(),
+//                'data' => $e->getTrace()],
+//                500);
+//        }
 
         return $this->shouldReturnJson($request, $e)
             ? $this->prepareJsonResponse($request, $e)
